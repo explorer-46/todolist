884023fad28d85c83f83112ee06153bd74564928
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-10 12:12:21 +0530

    hg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { AddlistComponent } from './addlist/addlist.component';
+
+const appRoutes: Routes=[
+  {path:"",component:AddlistComponent}
+
+]
+
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { AddlistComponent } from './addlist/addlist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
