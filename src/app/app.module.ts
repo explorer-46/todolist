c45432ffce6550df8f6011f34eafb870cf69c98f
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-10 11:46:05 +0530

    jgv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddlistComponent } from './addlist/addlist.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddlistComponent
+    AddlistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
